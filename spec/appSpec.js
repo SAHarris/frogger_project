571f3978b9526b358e6ad0ab0abf88e3c15a8197
Stuart Harris <stuartaharris@gmail.com>
2017-04-09 15:12:28 -0500

    create spec for app.js tests

---
+++ spec/appSpec.js
@@ -0,0 +1,11 @@
+describe('Enemy', function(){
+  var enemy;
+
+  it('should be able to create new Enemy', function(){
+    enemy = new Enemy(thisEnemy);
+  });
+  it('should be able to apply an emeny', function() {
+    
+  });
+
+});
