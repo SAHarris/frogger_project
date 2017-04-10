630bb10da0618f86dc4ae8b543c276813822aa83
Stuart Harris <stuartaharris@gmail.com>
2017-04-09 20:22:08 -0500

    add move function test

--- spec/appSpec.js
+++ spec/appSpec.js
@@ -1,11 +1,14 @@
 describe('Enemy', function(){
   var enemy,
     player;
-  it('should be able to create new Enemy', function(){
+  it('should be able to create new Enemy and Player', function(){
     enemy = new Enemy();
     player = new Player();
 
-    expect();
+    expect(enemy).toBeDefined();
+    expect(enemy.move).toBeDefined();
+    expect(player).toBeDefined();
+    expect(player.move).toBeDefined();
   });
   it('should be able to apply an enemy', function() {
 
