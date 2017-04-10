302a1537d9289e27fb0a0680231c4650154e8b42
Stuart Harris <stuartaharris@gmail.com>
2017-04-10 00:19:54 -0500

    update test suite

--- spec/appSpec.js
+++ spec/appSpec.js
@@ -1,17 +1,27 @@
-describe('Enemy', function(){
-  var enemy,
-    player;
-  it('should be able to create new Enemy and Player', function(){
-    enemy = new Enemy();
-    player = new Player();
+describe('Test suite for individual game funcrions:', function() {
 
-    expect(enemy).toBeDefined();
-    expect(enemy.move).toBeDefined();
-    expect(player).toBeDefined();
-    expect(player.move).toBeDefined();
+
+  describe('Enemy', function(){
+    var enemy;
+    it('should be able to create new Enemy', function(){
+      enemy = new Enemy();
+
+
+      expect(enemy).toBeDefined();
+      expect(enemy.move).toBeDefined();
+
+      });
+    });
+
+
+
+  describe('Player', function() {
+    var player;
+    it('should be able to create a Player', function() {
+      player = new Player();
+      expect(player).toBeDefined();
+      
+
+    });
   });
-  it('should be able to apply an enemy', function() {
-
-  });
-
 });
